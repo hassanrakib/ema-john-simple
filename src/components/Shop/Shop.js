efde545dcff15a148c2a33b0c838f41668a3e9e4
Rakib Hassan <mailrakibhassan@gmail.com>
2023-02-03 23:04:19 +0600

    shop created

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import './Shop.css';
+export default function Shop() {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h4>This is products container</h4>
+            </div>
+            <div className="cart-container">
+                <h4>This is shop container</h4>
+            </div>
+        </div>
+    )
+}
