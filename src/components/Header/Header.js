633cdf2c0a2ef144f98d357250ec2f24586c2ac3
Rakib Hassan <mailrakibhassan@gmail.com>
2023-02-07 12:32:11 +0600

    link setup done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../../images/Logo.svg";
 import './Header.css';
 function Header() {
@@ -5,10 +6,10 @@ function Header() {
         <nav className="header">
             <img src={logo} alt="ema-john logo" />
             <div>
-                <a href="/shop">Shop</a>
-                <a href="/orders">Orders</a>
-                <a href="/inventory">Inventory</a>
-                <a href="/about">About</a>
+                <Link to="/shop">Shop</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/about">About</Link>
             </div>
         </nav>
     )
