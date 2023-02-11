72fd6ec9956c5e1d4f1c16914a1cb6235217aebb
Rakib Hassan <mailrakibhassan@gmail.com>
2023-02-11 19:17:24 +0600

    login route design complete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Login.css";
 
 const Login = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>Login</h2>
-            <form></form>
+            <form onSubmit={handleLogin}>
+                <div className="form-control">
+                    <p><label htmlFor="email">Email:</label></p>
+                    <input id="email" type="email" name="email" placeholder='email' required/>
+                </div>
+                <div className="form-control">
+                    <p><label htmlFor="password">Password:</label></p>
+                    <input id="password" type="password" name="password" placeholder='password' required/>
+                </div>
+                <div className="form-control">
+                    <input type="submit" value="Login" />
+                </div>
+            </form>
+            <p>New to Ema John? <Link to="/signup">Create New Account</Link></p>
         </div>
     );
 };
